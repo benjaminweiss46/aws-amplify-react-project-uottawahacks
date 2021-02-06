50818a0d1650f559b63639672b2e827aee23fa95
Benjamin Weiss <benjamin.weiss46@gmail.com>
2021-02-05 21:41:22 -0500

    Adding a google map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { map, GoogleApiWrapper } from 'google-maps-react';
 
 function App() {
   return (
@@ -9,8 +10,24 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
 
+render() {
+    return (
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
+    );
+  }
+export default GoogleApiWrapper({
+  apiKey: 'TOKEN HERE'
+})(MapContainer);
 export default App;
