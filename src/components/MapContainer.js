0597fa3eca087fc00feb4b74f1a8b38241c113a5
Benjamin Weiss <benjamin.weiss46@gmail.com>
2021-02-06 02:05:13 -0500

    Add shape on map

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper, Polygon} from 'google-maps-react';
 
 const mapStyles = {
   width: '100%',
@@ -8,6 +8,12 @@ const mapStyles = {
 
 export class MapContainer extends Component {
   render() {
+    const triangleCoords = [
+      {lat: 25.774, lng: -80.190},
+      {lat: 18.466, lng: -66.118},
+      {lat: 32.321, lng: -64.757},
+      {lat: 25.774, lng: -80.190}
+    ];
     return (
       <Map
         google={this.props.google}
@@ -19,8 +25,16 @@ export class MapContainer extends Component {
             lng: -75.6972
           }
         }
-        onClick={this.onMapClicked} 
-      />
+        onClick={this.onMapClicked}>
+
+        <Polygon
+          paths={triangleCoords}
+          strokeColor="#0000FF"
+          strokeOpacity={0.8}
+          strokeWeight={2}
+          fillColor="#0000FF"
+          fillOpacity={0.35} />
+      </Map>
     );
   }
   onMapClicked() {
