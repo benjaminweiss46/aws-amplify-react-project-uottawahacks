c0c128dc8ab1307ec3603562be421b5fc71a93c6
Benjamin Weiss <benjamin.weiss46@gmail.com>
2021-02-06 00:04:08 -0500

    trying to add search box

--- src/App.js
+++ src/App.js
@@ -26,6 +26,30 @@ export class MapContainer extends Component {
   }
 }
 
+export default class SearchBox extends React.Component {
+  static propTypes = {
+    placeholder: React.PropTypes.string,
+    onPlacesChanged: React.PropTypes.func
+  }
+  render() {
+    return <input ref="input" {...this.props} type="text"/>;
+  }
+  onPlacesChanged = () => {
+    if (this.props.onPlacesChanged) {
+      this.props.onPlacesChanged(this.searchBox.getPlaces());
+    }
+  }
+  componentDidMount() {
+    var input = ReactDOM.findDOMNode(this.refs.input);
+    this.searchBox = new google.maps.places.SearchBox(input);
+    this.searchBox.addListener('places_changed', this.onPlacesChanged);
+  }
+  componentWillUnmount() {
+    // https://developers.google.com/maps/documentation/javascript/events#removing
+    google.maps.event.clearInstanceListeners(this.searchBox);
+  }
+}
+
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyBraKNh5eY4BQxe-xcfc4DhC5ZX_coTegs'
 })(MapContainer);
