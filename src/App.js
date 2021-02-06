559f94ca6c4d72f334a4801c2aa49c2aebb09054
Benjamin Weiss <benjamin.weiss46@gmail.com>
2021-02-05 21:59:42 -0500

    Adding map try 2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { map, GoogleApiWrapper } from 'google-maps-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-      <body>
-
-      </body>
-    </div>
-  );
+  export class MapContainer extends Component {
+    render() {
+      return (
+        <Map google={this.props.google} zoom={14}>
+   
+          <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+   
+          <InfoWindow onClose={this.onInfoWindowClose}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+          </InfoWindow>
+        </Map>
+      );
+    }
 }
-
-render() {
-    return (
-        <Map
-          google={this.props.google}
-          zoom={8}
-          style={mapStyles}
-          initialCenter={{ lat: 47.444, lng: -122.176}}
-        />
-    );
-  }
+ 
 export default GoogleApiWrapper({
-  apiKey: 'TOKEN HERE'
-})(MapContainer);
-export default App;
+  apiKey: (AIzaSyBraKNh5eY4BQxe-xcfc4DhC5ZX_coTegs)
+})(MapContainer)
+
+
