645ef9e1db3c894979d58cacf43cc7f56671359f
Benjamin Weiss <benjamin.weiss46@gmail.com>
2021-02-05 22:03:41 -0500

    maps try 3

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-function App() {
-  export class MapContainer extends Component {
-    render() {
-      return (
-        <Map google={this.props.google} zoom={14}>
-   
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-   
-          <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div>
-          </InfoWindow>
-        </Map>
-      );
-    }
-}
- 
-export default GoogleApiWrapper({
-  apiKey: (AIzaSyBraKNh5eY4BQxe-xcfc4DhC5ZX_coTegs)
-})(MapContainer)
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
 
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: -1.2884,
+            lng: 36.8233
+          }
+        }
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBraKNh5eY4BQxe-xcfc4DhC5ZX_coTegs'
+})(MapContainer);
 
